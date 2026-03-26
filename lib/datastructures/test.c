b1bc5f19478cd3c8fece1fcd37e56e2e2f22c4b9
Weiqi_incenteev <weiqi.zhang@incenteev.com>
2026-03-26 11:14:15 +0100

    test: add assertions to darray tests

--- lib/datastructures/test.c
+++ lib/datastructures/test.c
@@ -10,7 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include "libft.h"
 #include "gc_libft.h"
 #include "datastructures.h"
 
@@ -23,74 +25,79 @@ void	test_darray(t_gc *gc)
 {
 	t_darray	*list;
 
-	printf("==========darray test==========\n");
+	ft_printf("==========darray test==========\n");
 	list = init_darray(gc);
-	printf("Test empty darray\n");
-	list->repr(list, repr_str);
-	printf("\n");
 
-	printf("Test pop() from empty darray\n");
-	list->pop(list);
+	ft_printf("Test empty darray\n");
 	list->repr(list, repr_str);
-	printf("\n");
+	ft_printf("\n");
 
-	printf("Test pop_i() from empty darray\n");
-	list->pop_i(list, 1);
+	ft_printf("Test pop() from empty darray\n");
+	assert(list->pop(list) == NULL);
 	list->repr(list, repr_str);
-	printf("\n");
+	ft_printf("\n");
 
-	printf("Test peep() from empty darray\n");
-	list->peep(list);
+	ft_printf("Test pop_i() from empty darray\n");
+	assert(list->pop_i(list, 1) == NULL);
 	list->repr(list, repr_str);
-	printf("\n");
+	ft_printf("\n");
 
-	printf("Test insert(OUT_OF_RANGE_INDEX)\n");
+	ft_printf("Test peep() from empty darray\n");
+	assert(list->peep(list) == NULL);
+	list->repr(list, repr_str);
+	ft_printf("\n");
+
+	ft_printf("Test insert(OUT_OF_RANGE_INDEX)\n");
 	list->insert(list, 50000, gc_strdup("OUT_OF_RANGE_INDEX test", gc));
+	assert(list->len == 0);
 	list->repr(list, repr_str);
-	printf("\n");
+	ft_printf("\n");
 
-	printf("Test insert(NEGATIVE_INDEX)\n");
+	ft_printf("Test insert(NEGATIVE_INDEX)\n");
 	list->insert(list, -5, gc_strdup("NEGATIVE_INDEX test", gc));
+	assert(list->len == 0);
 	list->repr(list, repr_str);
-	printf("\n");
+	ft_printf("\n");
 
-	printf("Push 4 strs to list\n");
+	ft_printf("Push 4 strs to list\n");
 	list->push(list, gc_strdup("Hello", gc));
 	list->push(list, gc_strdup("World", gc));
 	list->push(list, gc_strdup("Mine", gc));
 	list->push(list, gc_strdup("Craft", gc));
+	assert(list->len == 4);
 	list->repr(list, repr_str);
-	printf("\n");
+	ft_printf("\n");
 
-	printf("Test peep()\n");
-	printf("Peep result: %s\n", (char *)list->peep(list));
+	ft_printf("Test peep() returns last element\n");
+	assert(strcmp((char *)list->peep(list), "Craft") == 0);
 	list->repr(list, repr_str);
-	printf("\n");
+	ft_printf("\n");
 
-	printf("Test pop()\n");
-	list->pop(list);
+	ft_printf("Test pop()\n");
+	assert(strcmp((char *)list->pop(list), "Craft") == 0);
 	list->repr(list, repr_str);
-	printf("\n");
+	ft_printf("\n");
 
-	printf("Test pop_i(1)\n");
-	list->pop_i(list, 1);
+	ft_printf("Test pop_i(1)\n");
+	assert(strcmp((char *)list->pop_i(list, 1), "World") == 0);
 	list->repr(list, repr_str);
-	printf("\n");
+	ft_printf("\n");
 
-	printf("Test pop_i(OUT_OF_RANGE_INDEX)\n");
-	list->pop_i(list, 5000);
+	ft_printf("Test pop_i(OUT_OF_RANGE_INDEX)\n");
+	assert(list->pop_i(list, 5000) == NULL);
 	list->repr(list, repr_str);
-	printf("\n");
+	ft_printf("\n");
 
-	printf("Test pop_i(NEGATIVE_INDEX)\n");
-	list->pop_i(list, -50000);
+	ft_printf("Test pop_i(NEGATIVE_INDEX)\n");
+	assert(list->pop_i(list, -50000) == NULL);
 	list->repr(list, repr_str);
-	printf("\n");
+	ft_printf("\n");
 }
 
 int	main(void)
 {
 	t_gc	*gc;
+
 	gc = init_gc();
 	test_darray(gc);
 	gc->clean(gc);

1d666685fcaa2189fc2ebb3090d79475e1760822
Weiqi_incenteev <weiqi.zhang@incenteev.com>
2026-03-26 13:15:52 +0100

    feat: parse token list into AST with Pratt parsing

--- src/parser.c
+++ src/parser.c
@@ -12,11 +12,62 @@
 
 #include <stdbool.h>
 #include <stddef.h>
+#include "datastructures.h"
 #include "gc_libft.h"
 #include "minishell.h"
 
 bool			is_valid_cmd(char *cmd);
 t_darray		*tokenize(char *cmd, t_gc *gc);
+
+static t_btree	*parse_recur(t_darray *tokens, size_t *i, int min_bp);
+
+static t_btree	*join_op(t_btree *left, t_darray *tokens, size_t *i)
+{
+	t_token	*op;
+	t_btree	*node;
+
+	op = tokens->peek_i(tokens, *i);
+	(*i)++;
+	node = init_btree(op, tokens->gc);
+	node->left = left;
+	node->right = parse_recur(tokens, i, op->bind_right);
+	return (node);
+}
+
+static t_btree	*parse_recur(t_darray *tokens, size_t *i, int min_bp)
+{
+	t_token	*token;
+	t_btree	*node;
+
+	if (*i >= tokens->len)
+		return (NULL);
+	token = tokens->peek_i(tokens, *i);
+	if (token->type == SUBSHELL)
+	{
+		node = init_btree(token, tokens->gc);
+		(*i)++;
+		node->left = parse_recur(tokens, i, 0);
+	}
+	else
+		node = init_btree(token, tokens->gc);
+	(*i)++;
+	while (*i < tokens->len)
+	{
+		token = tokens->peek_i(tokens, *i);
+		if (token->type == CLOSE_PAREN || token->bind_left < min_bp)
+			break ;
+		node = join_op(node, tokens, i);
+	}
+	return (node);
+}
+
+static t_btree	*parse(t_darray *tokens)
+{
+	size_t	i;
+
+	i = 0;
+	return (parse_recur(tokens, &i, 0));
+}
 
 t_parser	*init_parser(t_gc *gc)
 {
@@ -25,5 +76,6 @@ t_parser	*init_parser(t_gc *gc)
 	parser = gc_malloc(sizeof(t_parser), gc);
 	parser->is_valid_cmd = is_valid_cmd;
 	parser->tokenize = tokenize;
+	parser->parse = parse;
 	return (parser);
 }

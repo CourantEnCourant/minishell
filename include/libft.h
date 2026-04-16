2ed128888aac3db8d1ba738f7eba715ff34bc031
Weiqi Zhang <weiqi.zhang_arthur@yahoo.com>
2026-04-16 22:35:35 +0200

    feat: add ft_dprintf to libft

--- include/libft.h
+++ include/libft.h
@@ -67,6 +67,7 @@ int		ft_lstsize(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *node);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
+int		ft_dprintf(int fd, const char *s, ...);
 int		ft_vdprintf(int fd, const char *s, va_list ap);
 char	*get_next_line(int fd);
 

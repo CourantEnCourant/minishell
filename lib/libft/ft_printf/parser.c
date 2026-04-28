8fa43bee141c9aaa0ada702bed12f92e320e3e4a
Weiqi Zhang <weiqi.zhang_arthur@yahoo.com>
2026-04-28 18:39:55 +0200

    fix: make ft_vdprintf compatible for mac and linux

--- lib/libft/ft_printf/parser.c
+++ lib/libft/ft_printf/parser.c
@@ -21,22 +21,22 @@ int	parse_pointer(int fd, void *ptr);
 int	parse_unsigned_int(int fd, unsigned int n);
 int	parse_unsigned_hex(int fd, unsigned int n, bool upper);
 
-int	parse_arg(int fd, char arg, va_list *args)
+int	parse_arg(int fd, char arg, va_list args)
 {
 	if (arg == 'c')
-		return (parse_char(fd, va_arg(*args, int)));
+		return (parse_char(fd, va_arg(args, int)));
 	if (arg == 's')
-		return (parse_str(fd, va_arg(*args, char *)));
+		return (parse_str(fd, va_arg(args, char *)));
 	if (arg == 'p')
-		return (parse_pointer(fd, va_arg(*args, void *)));
+		return (parse_pointer(fd, va_arg(args, void *)));
 	if (arg == 'd' || arg == 'i')
-		return (parse_int(fd, va_arg(*args, int)));
+		return (parse_int(fd, va_arg(args, int)));
 	if (arg == 'u')
-		return (parse_unsigned_int(fd, va_arg(*args, unsigned int)));
+		return (parse_unsigned_int(fd, va_arg(args, unsigned int)));
 	if (arg == 'x')
-		return (parse_unsigned_hex(fd, va_arg(*args, unsigned int), false));
+		return (parse_unsigned_hex(fd, va_arg(args, unsigned int), false));
 	if (arg == 'X')
-		return (parse_unsigned_hex(fd, va_arg(*args, unsigned int), true));
+		return (parse_unsigned_hex(fd, va_arg(args, unsigned int), true));
 	if (arg == '%')
 	{
 		ft_putchar_fd('%', fd);

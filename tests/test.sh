17b83552d4b5815870cc629b4d5349b21b1670a9
Weiqi Zhang <weiqi.zhang_arthur@yahoo.com>
2026-05-26 19:25:05 +0200

    tests: update test to only include minishell_tester

--- tests/test.sh
+++ tests/test.sh
@@ -1,16 +1,7 @@
-# test_error_handling
-touch no_permisson.tmp
-chmod 000 no_permisson.tmp
-
-# Launching tests
-./test
-
-# Cleaning
-rm -f *.tmp
-
 # minishell_tester
 make all
 git clone https://github.com/LucasKuhn/minishell_tester.git
 cd minishell_tester
 ./tester
 cd ..
+rm -rf minishell_tester

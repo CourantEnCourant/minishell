7a19f39fa7eb39a90f32e24872d6c6494ba1f1f3
Weiqi Zhang <weiqi.zhang_arthur@yahoo.com>
2026-05-27 17:37:56 +0200

    fix: will exit when not numerical argument as in bash

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -33,7 +33,7 @@ static bool	check_args(char **args, t_env *env)
 		ft_dprintf(STDERR_FILENO,
 			"exit: %s: numeric argument required\n", args[1]);
 		env->exit_code = 2;
-		return (false);
+		return (true);
 	}
 	if (len((void **)args) > 2)
 	{
